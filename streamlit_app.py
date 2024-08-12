0b19f98f3572a7408996d545d332838c789aa53f
daperezmora <72536458+daperezmora@users.noreply.github.com>
2024-08-12 16:40:04 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,6 @@ import requests
 import matplotlib.pyplot as plt
 from datetime import datetime, timedelta
 import pytz
-import time
 
 # Variables para el ID del sensor y el token
 token = '9b56e023d84c4c0e9af2d0ee95549392'
@@ -14,11 +13,15 @@ timezone = pytz.timezone('America/Mexico_City')
 
 st.title("Dashboard en tiempo real - Radiación Solar")
 
+# Inicializar variables para almacenar todos los datos
+all_date_times = []
+all_solar_radiation_data = []
+
 # Definir una función para obtener el último dato registrado del sensor
 def get_last_recorded_data():
-    # Configurar el tiempo de inicio como 5 minutos atrás desde el tiempo actual en la zona horaria del servidor
+    # Configurar el tiempo de inicio como 15 minutos atrás desde el tiempo actual en la zona horaria del servidor
     now = datetime.now(timezone)
-    dt_start = (now - timedelta(minutes=5)).strftime('%Y-%m-%d %H:%M:%S')
+    dt_start = (now - timedelta(minutes=15)).strftime('%Y-%m-%d %H:%M:%S')
     dt_end = now.strftime('%Y-%m-%d %H:%M:%S')
 
     # Construir la URL de la API con los parámetros necesarios
@@ -47,27 +50,34 @@ def get_last_recorded_data():
 
 # Mostrar los datos en un gráfico
 def display_data():
+    global all_date_times, all_solar_radiation_data
+
     dateTime, solar_radiation_data = get_last_recorded_data()
 
     if dateTime:
+        # Agregar los nuevos datos a las listas
+        all_date_times.extend(dateTime)
+        all_solar_radiation_data.extend(solar_radiation_data)
+
         st.subheader("Gráfico de Radiación Solar en Tiempo Real")
 
+        # Crear la figura y los ejes
         fig, ax = plt.subplots()
-        ax.plot(dateTime, solar_radiation_data, 'g-o')
+
+        # Limpiar la gráfica anterior
+        ax.clear()
+
+        # Dibujar la nueva gráfica con todos los datos
+        ax.plot(all_date_times, all_solar_radiation_data, 'g-o')
         ax.set_title('Radiación Solar vs. Tiempo')
         ax.set_xlabel('Tiempo')
         ax.set_ylabel('Radiación Solar (W/m^2)')
         ax.grid(True)
 
+        # Mostrar la gráfica en Streamlit
         st.pyplot(fig)
     else:
         st.info("Sin datos disponibles para el rango de tiempo especificado.")
-
-# Actualización automática del dashboard cada cierto tiempo
-def auto_update(interval=60):
-    while True:
-        display_data()
-        time.sleep(interval)
 
 # Botón para actualizar los datos manualmente
 if st.button("Actualizar Datos"):
@@ -75,7 +85,3 @@ if st.button("Actualizar Datos"):
 
 # Mostrar los datos al cargar la página
 display_data()
-
-# Actualización automática (puedes ajustar el intervalo en segundos)
-if st.checkbox("Actualizar automáticamente cada 60 segundos"):
-    auto_update(60)
